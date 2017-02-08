3df9724653187ddcb37be49352e5b2e497be8482
Michael Willis <micwillis@gmail.com>
2017-02-08 14:29:39 -0600

    Add function wrapper around $Profile

---
+++ ProfileCaddy/private/Get-ProfilePath.ps1
@@ -0,0 +1,23 @@
+<#
+.Example
+Get-ProfilePath
+Get-ProfilePath -Name CurrentUserCurrentHost
+
+.Notes
+https://blogs.technet.microsoft.com/heyscriptingguy/2012/05/21/understanding-the-six-powershell-profiles/
+#>
+function Get-ProfilePath
+{
+    [cmdletbinding()]
+    param(
+        [ValidateSet("AllUsersAllHosts", "AllUsersCurrentHost", "CurrentUserAllHosts", "CurrentUserCurrentHost")]
+        [string] $Name
+    )
+
+    if($Name -eq $null -or $Name -eq "")
+    {
+        return $profile | Select-Object AllUsersAllHosts, AllUsersCurrentHost, CurrentUserAllHosts, CurrentUserCurrentHost
+    }
+
+    return $Profile."$Name"
+}
