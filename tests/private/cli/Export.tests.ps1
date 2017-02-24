f8f2a77564e701320a8abe6ff06b95d359612244
Michael Willis <micwillis@gmail.com>
2017-02-24 09:14:12 -0600

    Debug paths.

--- tests/private/cli/Export.tests.ps1
+++ tests/private/cli/Export.tests.ps1
@@ -1,10 +1,10 @@
 $pester = & (Resolve-Path ".\ProfileCaddie.Pester.ps1") $MyInvocation.MyCommand.Path
-
+$pester
 Describe $pester.Namespace {
     InModuleScope $pester.ModuleName {
         Context "Basic Export" {
             It "Copies the gist.json file to target path" {
-                Mock Resolve-UncertainPath { return "TestDrive:/.pscaddie" } -ParameterFilter { "~/.pscaddie" }
+                Mock Resolve-UncertainPath { return "TestDrive:/.pscaddie" } -ParameterFilter { $Path -eq "~/.pscaddie" }
                 Mock Copy-Item {} -Verifiable
                 Init
                 Export -Path "./gists.json"
